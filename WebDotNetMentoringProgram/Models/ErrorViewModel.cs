cf29c839b6d2fd285b43a1556863b181d8dbf891
Attila Szallas <ASzallas@slb.com>
2023-04-05 14:05:26 +0200

    Add project files.

---
+++ WebDotNetMentoringProgram/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebDotNetMentoringProgram.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
