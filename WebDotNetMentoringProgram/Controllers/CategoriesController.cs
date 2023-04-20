e8b4688d8bb8dd1d845739374e8c71597c1fd8dd
Attila Szallas <ASzallas@slb.com>
2023-04-21 00:31:07 +0200

    Fix merge conflict

--- WebDotNetMentoringProgram/Controllers/CategoriesController.cs
+++ WebDotNetMentoringProgram/Controllers/CategoriesController.cs
@@ -22,79 +22,9 @@ namespace WebDotNetMentoringProgram.Controllers
                 return View(_categories.GetCategories());
             }
             else
-            { 
+            {
                 return Problem($"Entity set '{nameof(CategoryRepository)}' is null.");
             }
-
-            var category = await _context.Categories.FindAsync(id);
-
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return View(category);
-        }
-
-        // POST: Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CategoryId,CategoryName,Description")] Category category)
-        {
-            if (id != category.CategoryId)
-            {
-                return BadRequest();
-            }
-
-            if (ModelState.IsValid)
-            {
-                _context.Update(category);
-                await _context.SaveChangesAsync();
-
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
-
-        // GET: Categories/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return BadRequest();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
-        // POST: Categories/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (id == null)
-            {
-                return BadRequest();
-            }
-
-            var category = await _context.Categories.FindAsync(id);
-            
-            if (category != null)
-            {
-                _context.Categories.Remove(category);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
     }
 }
