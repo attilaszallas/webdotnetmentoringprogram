7938168326225d0dd37f56651704cd8495f5f91e
Attila Szallas <ASzallas@slb.com>
2023-05-11 21:42:06 +0200

    Module Razor and Views, Task 2. ViewComponents / PartialViews

---
+++ WebDotNetMentoringProgram/Components/BreadCrumb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDotNetMentoringProgram.ViewComponents
+{
+    public class BreadCrumb : ViewComponent
+    {
+        private static string _breadCrumb ;
+
+        public IViewComponentResult Invoke()
+        {
+            var request = HttpContext.Request;
+            string controller = request.RouteValues["controller"].ToString();
+            string action = request.RouteValues["action"].ToString();
+
+            if (_breadCrumb != null)
+            {
+                _breadCrumb = _breadCrumb + " > ";
+            }
+
+            _breadCrumb = _breadCrumb + ( (action == "Index")
+                ? controller
+                : action);
+
+            if (controller == "Home" && action == "Index")
+            { 
+                return Content(string.Empty);
+            }
+
+            ViewBag.BreadCrumb = _breadCrumb;
+
+            return View();
+        }
+    }
+}
