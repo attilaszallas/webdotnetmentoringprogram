5a30bd699eda45db7877ae92e1a4bcbfacdeec66
Attila Szallas <ASzallas@slb.com>
2023-05-12 15:12:05 +0200

    Module Razor and Views, Task 3. Html Helpers / Html Tags (Tag Helpers)

---
+++ WebDotNetMentoringProgram/Helpers/ImageHtmlHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace WebDotNetMentoringProgram.Helpers
+{
+    public static class ImageLinkHelper
+    {
+        public static IHtmlContent ImageLink(this HtmlHelper helper, int imageId, string text)
+        {
+            string htmlString = string.Format("<a href='Images/{0}'>{1}</a>", imageId, text);
+
+            return new StringHtmlContent(htmlString);
+        }
+    }
+}
---
+++ WebDotNetMentoringProgram/TagHelpers/ImageLinkTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebDotNetMentoringProgram.TagHelpers
+{
+    public class ImageLinkTagHelper : TagHelper
+    {
+        public int ImageId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "Images/" + ImageId);
+        }
+    }
+}
